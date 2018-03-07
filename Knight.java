680e53d55673d9db4535f742605a894b4f1f8c77
Nityam Jigyasu <nmj18@txstate.edu>
2018-03-07 17:44:35 -0600

    Knight moves and methods

--- Knight.java
+++ Knight.java
@@ -1,13 +1,40 @@
-package chessPack;
+//Knight moves and methods
 
-public class Knight extends Piece {
+package Pieces;
 
-	private String moveType;
-	
-	public Knight(boolean taken, String image) {
-		super(taken, image);
-		// TODO Auto-generated constructor stub
-		moveType = "Knight";
-	}
-
+import Game.Player;
+import Game.Form;
+public class Knight extends Piece
+{
+  Form form;
+  
+  public Knight(int x, int y, Player player)
+  {
+    super(x, y, player);
+    form = Form.KNIGHT;
+  }
+  
+  public Form getForm()
+  {
+    return Form.KNIGHT;
+  }
+  
+  public boolean isValidPath(int lastX, int lastY)
+  {
+    int x_difference = Math.abs(finalX - x);
+    int y_difference = Math.abs(finalY - y);
+    
+    if (((x_difference == 1) && (y_difference == 2)) || ((x_difference == 2) && (y_difference == 1))) {
+      return true;
+    }
+    return false;
+  }
+  public int[][] drawPath(int firstX, int firstY, int lastX, int lastY)
+  {
+    int pairs = 0;
+    int[][] path = new int[2][pairs];
+    
+    return path;
+  }
 }
+
